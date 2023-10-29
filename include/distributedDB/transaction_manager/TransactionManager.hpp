937fcebf57aa22e6153f2bbc2f2c37adad400d64
meliodas137 <mayankgt1043@gmail.com>
2023-10-28 20:26:19 -0400

    initializing data managers and data items from tm

--- include/distributedDB/transaction_manager/TransactionManager.hpp
+++ include/distributedDB/transaction_manager/TransactionManager.hpp
@@ -11,24 +11,36 @@
 
 #pragma once
 
-#include <unordered_map>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include "distributedDB/data_manager/DataManager.hpp"
+#include "distributedDB/transaction_manager/transaction/Transaction.hpp"
 
 using namespace std;
 
 namespace distributedDB {
 class TransactionManager {
-  public:
-    TransactionManager(int dmCount, unordered_map<int, vector<int>> &dmToVarlist);
-    string beginTransaction(int transactionId);
-    string endTransaction(int transactionId);
-    string readData(int transactionId, int dataId);
-    string writeData(int transactionId, int dataId, int dataValue);
-    string recoverDataManager(int dataManagerId);
-    string failDataManager(int dataManagerId);
-    string dumpData();
-  
+private: 
+  vector<vector<int>> dmToVarList = {};
+  int dmCount;
+  vector<DataManager> managers = {};
+  unordered_map<int, vector<Transaction>> transactions;
+  int globalClock = 0;
+
+private:
+  int incrementClock();
+
+public:
+  TransactionManager(int dmCount, vector<vector<int>> &dmToVarlist);
+  string beginTransaction(int transactionId);
+  string endTransaction(int transactionId);
+  string readData(int transactionId, int dataId);
+  string writeData(int transactionId, int dataId, int dataValue);
+  string recoverDataManager(int dataManagerId);
+  string failDataManager(int dataManagerId);
+  string dumpData();
+
 };
 }
 #endif 
--- src/transaction_manager/TransactionManager.cpp
+++ src/transaction_manager/TransactionManager.cpp
@@ -1,14 +1,24 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <string>
 #include "distributedDB/transaction_manager/TransactionManager.hpp"
 
 using namespace std;
 
 namespace distributedDB {
-TransactionManager::TransactionManager(int dmCount, unordered_map<int, vector<int>> &dmToVarlist){};
-string TransactionManager::beginTransaction(int transactionId){ return "Not Implemented Error.";};
+
+TransactionManager::TransactionManager(int dmCount, vector<vector<int>> &dmToVarlist): dmCount(dmCount), dmToVarList(dmToVarlist){
+    for(int i = 0; i < dmCount; i++) {
+        // cout<<"creating manager "<<i<<endl;
+        managers.emplace_back(DataManager(dmToVarList[i]));
+    }
+}
+
+int TransactionManager::incrementClock() {
+    return ++globalClock;
+}
+
+string TransactionManager::beginTransaction(int transactionId) {
+
+    return "Not Implemented Error.";
+}
 string TransactionManager::endTransaction(int transactionId){ return "Not Implemented Error.";};
 string TransactionManager::readData(int transactionId, int dataId){ return "Not Implemented Error.";};
 string TransactionManager::writeData(int transactionId, int dataId, int dataValue){ return "Not Implemented Error.";};
